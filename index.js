33a748d59798d3ace713a9e931c3952cead7fccd
Julien Blancher <julien.blancher@recast.ai>
2016-12-15 17:08:15 +0100

    [ADD] minimal kit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+console.log('Loading function')
+
+// MODULES IMPORT
+const recast = require('recastai')
+
+// RECAST.AI INIT
+const recastClient = new recast.Client(process.env.REQUEST_TOKEN)
+
+exports.handler = (event, context, done) => {
+    console.log('Received event:', JSON.stringify(event, null, 2))
+    const text = (event.text === undefined ? null : event.text)
+
+    if (text) {
+      recastClient.textConverse(text, { conversationToken: process.env.CONVERSATION_TOKEN || null })
+        .then(recast => {
+          console.log(recast)
+          done(null, {
+            reply: recast.reply(),
+            conversationToken: recast.conversation_token
+          });
+        })
+        .catch(err => {
+          console.error(err)
+          done(err)
+        });
+    } else if (!text) {
+      done(new Error('No text provided'))
+    }
+}
