c2b9cb490670112a589e260ff18e2cc96e0170b9
Julien Blancher <julien.blancher@recast.ai>
2017-01-02 10:59:37 +0100

    [ADD] Lint eslint

--- index.js
+++ index.js
@@ -9,23 +9,22 @@ const recast = require('recastai')
 const recastClient = new recast.Client(process.env.REQUEST_TOKEN)
 
 exports.handler = (event, context, done) => {
-    console.log('Received event:', JSON.stringify(event, null, 2))
-    const text = (event.text === undefined ? null : event.text)
+  console.log('Received event:', JSON.stringify(event, null, 2))
+  const text = (event.text === undefined ? null : event.text)
 
-    if (text) {
-      recastClient.textConverse(text, { conversationToken: process.env.CONVERSATION_TOKEN || null })
-        .then(recast => {
-          console.log(recast)
-          done(null, {
-            reply: recast.reply(),
-            conversationToken: recast.conversation_token
-          });
-        })
-        .catch(err => {
-          console.error(err)
-          done(err)
+  if (text) {
+    recastClient.textConverse(text, { conversationToken: process.env.CONVERSATION_TOKEN || null })
+      .then(recast => {
+        done(null, {
+          reply: recast.reply(),
+          conversationToken: recast.conversation_token
         });
-    } else if (!text) {
-      done(new Error('No text provided'))
-    }
+      })
+      .catch(err => {
+        console.error(err)
+        done(err)
+      });
+  } else if (!text) {
+    done(new Error('No text provided'))
+  }
 }
