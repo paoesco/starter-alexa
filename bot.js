4f886c273f019af569d78f417cc102e627c8c167
Julien Blancher <julien.blancher@recast.ai>
2017-03-09 19:37:12 +0100

    [ADD] botconnector handling

--- bot.js
+++ bot.js
@@ -2,9 +2,26 @@ console.log('Loading function')
 
 // MODULES IMPORT
 const recast = require('recastai')
+const botConnector = require('recastai-botconnector')
 
 // RECAST.AI INIT
 const recastClient = new recast.Client(process.env.REQUEST_TOKEN)
+
+const myBot = new botConnector({ botId: process.env.BOT_ID, userSlug: process.env.USER_SLUG, userToken: process.env.USER_TOKEN })
+
+myBot.onTextMessage(message => {
+    const text = message.content.attachment.content
+    const senderId = message.senderId
+    const res = recastClient.textConverse(text, { conversationToken: senderId })
+
+    console.log(message)
+
+    res.then((r) => {
+      console.log(r)
+      r.replies.forEach(content => message.addReply({ type: 'text', content }))
+      message.reply()
+    })
+})
 
 exports.bot = function(event, context, done) {
   console.log('Received event:', JSON.stringify(event, null, 2))
@@ -22,7 +39,11 @@ exports.bot = function(event, context, done) {
         console.error(err)
         done(err)
       })
-  } else if (!text) {
+  } else if (event.message) {
+    const req = {}
+    req.body = event
+    myBot.listen(req)
+  } else {
     done(new Error('No text provided'))
   }
 }
