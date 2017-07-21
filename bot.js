3b6c77df73ca674176e780a068cb43036ab18a49
Julien Blancher <julien.blancher@recast.ai>
2017-07-21 11:32:09 +0200

    refactor(bot): updates

--- bot.js
+++ bot.js
@@ -1,49 +1,80 @@
 console.log('Loading function')
 
-// MODULES IMPORT
-const recast = require('recastai')
-const botConnector = require('recastai-botconnector')
+const r = require('recastai')
+const recast = new r.Client(process.env.REQUEST_TOKEN)
+const request = require('request')
 
-// RECAST.AI INIT
-const recastClient = new recast.Client(process.env.REQUEST_TOKEN)
+let sessionId = null
+let endConv = false
 
-const myBot = new botConnector({ botId: process.env.BOT_ID, userSlug: process.env.USER_SLUG, userToken: process.env.USER_TOKEN })
+let reply = (text, done) => {
+  done.send({
+    response: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: text
+      },
+      shouldEndSession: endConv
+    }
+  })
+  endConv = false
+}
 
-myBot.onTextMessage(message => {
-    const text = message.content.attachment.content
-    const senderId = message.senderId
-    const res = recastClient.textConverse(text, { conversationToken: senderId })
+let sendSMS = (body) => {
+  const twilioUrl = 'https://api.twilio.com/2010-04-01/Accounts/ACff4221972b56e6f75223039c9648fd49/Messages.json'
+  const to = '+33635917428'
+  const from = '+33756798311'
+  const appId = 'ACff4221972b56e6f75223039c9648fd49'
+  const appSecret = '78d40a89c33b453ed1aa128920971860'
 
-    console.log(message)
+  request.post(twilioUrl).form({'To': to, 'From': from, 'Body': body}).auth(appId, appSecret)
+}
 
-    res.then((r) => {
-      console.log(r)
-      r.replies.forEach(content => message.addReply({ type: 'text', content }))
-      message.reply()
-    })
-})
+let handleIntent = (res) => {
+  endIntents = [
+    'goodbye',
+    'thanks',
+  ]
 
-exports.bot = function(event, context, done) {
+  if (res.action && res.action.slug == 'sms-report') {
+    sendSMS("Your account balance is +$2340,25. Your last expense is $50 at Marc and Spensers LDT. Have a great day! Your banking assistant.")
+  } else if (res.action && endIntents.indexOf(res.action.slug) > -1) {
+    console.log('End of conversation')
+    endConv = true
+  }
+  return res.replies.join(' ')
+}
+
+exports.bot = (event, done) => {
   console.log('Received event:', JSON.stringify(event, null, 2))
-  const text = (event.text === undefined ? null : event.text)
 
-  if (text) {
-    recastClient.textConverse(text, { conversationToken: process.env.CONVERSATION_TOKEN || null })
-      .then((res) => {
-        done(null, {
-          reply: res.reply(),
-          conversationToken: res.conversationToken,
+  let helloReplies = [
+    'Hello! How can I help you today?',
+    'Welcome back. What do you need?'
+  ]
+
+  if (event.request.type == 'LaunchRequest') {
+    reply(helloReplies[Math.floor(Math.random() * helloReplies.length)], done)
+  } else if (event.request.type == 'IntentRequest') {
+    const text = event.request.intent.slots.sentence.value
+
+    console.log('Received: ', text)
+
+    sessionId = (sessionId == null ? event.session.sessionId : sessionId)
+
+    if (text) {
+      recast.textConverse(text, { conversationToken: sessionId })
+        .then((res) => {
+          console.log(res)
+          let rep = handleIntent(res)
+          reply(rep, done)
         })
-      })
-      .catch((err) => {
-        console.error(err)
-        done(err)
-      })
-  } else if (event.message) {
-    const req = {}
-    req.body = event
-    myBot.listen(req)
-  } else {
-    done(new Error('No text provided'))
+        .catch((err) => {
+          console.error(err)
+          done.send(err)
+        })
+    } else {
+      done.send(new Error('No text provided'))
+    }
   }
 }
---
+++ server.js
@@ -0,0 +1,30 @@
+/*
+ * server.js
+ * This file is the core of your bot
+ *
+ * It creates a little server using express
+ * So, your bot can be triggered thought "/" route
+ */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const bot = require('./bot').bot
+
+// Start Express server
+const app = express()
+app.set('port', 4000)
+app.use(bodyParser.json())
+
+// Handle / route
+app.use('/', (request, response) => {
+
+  // Call bot main function
+  bot(request.body, response)
+
+})
+
+// Run Express server, on right port
+app.listen(app.get('port'), () => {
+  console.log('Our bot is running on port', app.get('port'))
+})
